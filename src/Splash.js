34ea03c1f930c0da4cfcafc22d9a60a4d75217ed
gucci-ninja <beautyspell5@gmail.com>
2020-10-27 00:12:22 -0400

    Added user authentication

---
+++ src/Splash.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from '@react-native-firebase/app'
+import '@react-native-firebase/auth';
+// export default class Loading extends React.Component {
+//   componentDidMount() {
+//     firebase.auth().onAuthStateChanged(user => {
+//       this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+//     })
+//   }
+
+// export default class IntialView extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Loading</Text>
+//         <ActivityIndicator size="large" color="#0000ff" />
+//       </View>
+//     )
+//   }
+// }
+export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Home' : 'Signup')
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" color="blue"/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
