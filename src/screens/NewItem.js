9939e2b182902698f67f64c662db1db0eaa217d7
gucci-ninja <beautyspell5@gmail.com>
2020-11-08 17:23:44 -0500

    added new item screen

--- src/screens/NewItem.js
+++ src/screens/NewItem.js
@@ -11,23 +11,24 @@ export default class NewItem extends React.Component {
     console.log(props)
     super(props);
   }
-  state = { itemId: null, itemName: "" };
+  state = { itemId: null, itemName: "", details: "" };
 
-  submit = () => {
-    this.addItem(this.state.itemName);
-  }
-
-  addItem = itemName => {
+  addItem = async itemName => {
     const { drizzle, drizzleState } = this.props.screenProps;
     const contract = drizzle.contracts.ItemStore;
 
     // call the newItem method
-    const itemId = contract.methods["addItem"].cacheSend(itemName, {
-      from: drizzleState.accounts[0]
+    const itemId = await contract.methods["addItem"].cacheSend(itemName, {
+      from: drizzleState.accounts[0],
+      gas: 100000
     });
 
+    const details = contract.methods["getItem"].call(1, 2);
+
+
+
     // save the itemId for later
-    this.setState( { itemId });
+    this.setState( { itemId, details, itemName: '' });
   };
 
   render() {
@@ -44,7 +45,7 @@ export default class NewItem extends React.Component {
       <Button mode="contained" onPress={this.submit}>
         Add Item
       </Button>
-      <Title>{this.state.itemId}</Title>
+      <Title>{this.state.details}</Title>
       </Background>
     )
   }
