097ad175213f6347b98201ebd4c7a6bc251d0959
gucci-ninja <beautyspell5@gmail.com>
2021-02-02 02:31:37 -0500

    tested and confirmed elastic indexer works

--- elastic/index.js
+++ elastic/index.js
@@ -1,46 +1,15 @@
 var elasticsearch = require('elasticsearch');
-
-var Web3 = require("web3");
-var ItemStore = require("../build/contracts/ItemStore.json");
-const NODE_ADDRESS = `http://127.0.0.1:8545`;
-const web3 = new Web3(new Web3.providers.HttpProvider(NODE_ADDRESS));
+const ethClient = require("./processor");
 
 const itemBody = {
   "mappings": {
     "properties": {
-      "name": {
-        "type": "keyword"
-      },
-      "timesTracked": {
-        "type": "integer"
-      },
-      "creator": {
-        "type": "keyword"
-      },
-      "locations": {
+      "location": {
         "type": "geo_shape"
       },
-      "timestamps": {
-        "type": "date"
-      }
     }
   }
 }
-
-async function setContract() {
-    addr=(await web3.eth.getAccounts())[0]
-    console.log(addr);
-    const networkId = await web3.eth.net.getId()
-    const deployedAddress = ItemStore.networks[networkId].address
-    var ItemContract = new web3.eth.Contract(ItemStore.abi, deployedAddress);
-    //ItemContract.methods.addItem("grapes", "12345", "-69,69").send({from:addr, gas: 6721975});
-    // out = await ItemContract.methods.getItems().call().send({from:addr, gas: 6721975});
-    const out = await ItemContract.methods.scanItem(0).call()
-    //const out = await ItemContract.methods.total_items.call().call()
-    console.log(out);
-}
-
-
 
 // Connect to ES Client
 const elasticsearchClient = new elasticsearch.Client({
@@ -77,7 +46,7 @@ function deleteIndex(indexName) {
 function addDocument(indexName, itemId, docBody) {
   elasticsearchClient.index({  
     index: indexName,
-    id: itemId,
+    id: parseInt(itemId)+1,
     type: '_doc',
     body: docBody
   },function(err,resp,status) {
@@ -85,38 +54,85 @@ function addDocument(indexName, itemId, docBody) {
   });
 }
 
+async function postTransactions(transaction) {
+  if (transaction.length === 0) {
+    console.log("\tempty block - skipping");
+    return;
+  }
+  addDocument('item', transaction.itemId, transaction)
 
-// elasticsearchClient.cluster.health({},function(err,resp,status) {  
-//     console.log("-- Client Health --",resp);
-//   });
+  // const t = await ethClient.get
+  // Get 
+	// const bulkBody = transactions
+	// 	.map(createBulkEntry)
+	// 	.reduce((a, b) => a.concat(b), []);
 
-async function getBlock() {
-    const block = await web3.eth.getBlockNumber();
-    console.log(block)
+	// elasticsearchClient.bulk({
+	// 	body: bulkBody
+	// }, (err, resp) => {
+	// 	if (err) {
+	// 		console.log("\terror sending bulk: ", err);
+	// 	} else {
+	// 		console.log("\tbulk sent successfully");
+	// 	}
+	// });
 }
+
+async function iterateBlocks(current, end) {
+
+	const latestBlockNumber = await ethClient.getLastBlockNumber();
+
+	end = Math.max(latestBlockNumber, end || latestBlockNumber);
+	while (current <= end) {
+		try {
+      postTransactions(await ethClient.processBlock(current));
+			current++;
+		} catch (e) {
+			console.log(`failed to retrieve last block: ${e}`);
+			return current;
+		}
+	}
+	return current;
+}
+
+async function main() {
+  // createIndex('item') 
+  await ethClient.setContract();
+	let resolvePromise;
+	const promise = new Promise(resolve => {
+		resolvePromise = resolve;
+	});
+
+	const endBlock = await ethClient.getLastBlockNumber();
+	const startBlock = 7;
+
+	let lastProcessed = startBlock;
+	const thread = async () => {
+    lastProcessed = await iterateBlocks(lastProcessed, endBlock);
+
+		if (lastProcessed) {
+			setTimeout(thread, 10000);
+		} else {
+			resolvePromise();
+		}
+	}
+	await thread();
+
+	return promise;
+}
+
+main().then(() => console.log("Your blockchain is fully synced!"));
 
 
 // setContract();
 // deleteIndex('item')
-createIndex('item')
-addDocument('item', '1', {
-  "name": "crabs",
-  "locations": { 
-    "coordinates": [ [-80.12, 43.34], [-60.8264672, 49.129836], [-29.10, 79.123]], 
-    "type": "linestring" 
-  }  
-}
-)
+// createIndex('item')
+// addDocument('item', '1', {
+//   "name": "crabs",
+//   "locations": { 
+//     "coordinates": [ [-80.12, 43.34], [-60.8264672, 49.129836], [-29.10, 79.123]], 
+//     "type": "linestring" 
+//   }  
+// }
+// )
 
-// put example3
-// {
-//     "mappings": {
-//       "properties": {
-//         "location": {
-//           "type": "geo_shape"
-//         }
-//       }
-//     }
-//   }
-
-// put example3/_doc/4
