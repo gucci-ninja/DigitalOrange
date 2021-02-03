4b9cc79828a0959302cb947a6605da4d24deed6f
gucci-ninja <beautyspell5@gmail.com>
2021-02-03 00:42:56 -0500

    using flatlist to show history

--- src/screens/History.js
+++ src/screens/History.js
@@ -19,79 +19,17 @@ class History extends React.Component {
     dateStackId: null
   }
 
-  constructor(props){
-    console.log('constructor for history');
-    console.log(props.navigation.state);
-    super()
-    this.history = []
-    setTimeout(() => {      
-      this.data = [
-        {time: 'Feb 19, 2:42PM', title: '-50.371637,-16.299051', circleColor: '#009688',lineColor:'#009688'},
-        {time: 'Feb 19, 2:45PM', title: '-102.917438,44.590467'},
-        {time: 'Feb 19, 2:49PM', title: '42.682435,-84.61776'},
-        {time: `Feb 19, ${ new Date().getHours()-12}:${ new Date().getMinutes()}PM`, title: '-84.617760,42.682435',lineColor:'#009688'},
-      ]
-      this.name = 'Oranges' }, 1500);
-   // x.each_with_index { |key,index| z << { date: key, minutes: y[index]} }
-
-  }
-
-  // componentDidMount() {
-
-  //   if (this.props.isFocused) {
-  //     console.log('running mount')
-
-  //     // Use the `this.props.isFocused` boolean
-  //     // Call any action
-  //     console.log(this.props.isFocused);
-  //     if (this.props.isFocused) {
-  //       if (this.props.navigation.state.params){
-  //         console.log(this.props.navigation.state.params)
-  //       this.itemId = this.props.navigation.state.params.itemId || 0;
-  //       this.getHistory();
-  //       }
-  //     }
-  //   }
-  // }
-
-  // componentDidUpdate(prevProps) {
-  //   if (prevProps.isFocused !== this.props.isFocused) {
-  //     console.log('running update')
-
-  //     // Use the `this.props.isFocused` boolean
-  //     // Call any action
-  //     console.log(this.props.isFocused);
-  //     if (this.props.isFocused) {
-  //       if (this.props.navigation.state.params){
-  //         console.log(this.props.navigation.state.params)
-  //       this.itemId = this.props.navigation.state.params.itemId || 0;
-  //       this.getHistory();
-  //       }
-  //     }
-  //   }
-  // }
-
-  // clearParams = () => {
-  //   this.props.navigation.setParams({itemId: null})
-  // }
-
   getHistory  = async (payload) => {
-    console.log(payload.state.params);
-    if (!payload.state || !payload.state.params || payload.state.params.itemId == null) console.log('uhoh')
     if (!payload.state || !payload.state.params || payload.state.params.itemId == null) return;
-    console.log(payload.state.params.itemId);
 
     const { drizzle, drizzleState } = this.props.screenProps;
     const contract = drizzle.contracts.ItemStore;
 
-    // call the newItem method
+    // call the contract method
     const locationStackId = await contract.methods["getLocations"].cacheSend(payload.state.params.itemId, {
       from: drizzleState.accounts[0],
       gas: 6721975
     });
-
-    console.log(drizzle.store.getState().transactionStack);
-    //const locations = await this.getEventResponse(locationStackId);
 
     const dateStackId = await contract.methods["getDates"].cacheSend(payload.state.params.itemId, {
       from: drizzleState.accounts[0],
@@ -99,27 +37,19 @@ class History extends React.Component {
     });
 
     this.setState({ locationStackId, dateStackId });
-    // const dates = await this.getEventResponse(dateStackId);
-
-    // console.log(dates)
-    // console.log(locations)
   }
 
   getEventResponse = () => {
-    // console.log(this.props.screenProps.drizzle.store.getState().transactionStack)
     if (!this.state.locationStackId ||!this.state.dateStackId) return [];
     const { transactions, transactionStack } = this.props.screenProps.drizzleState;
 
-
-    // get the transaction hash usisng stack id
+    // get the transaction hashes usisng stack ids
     const locationHash = transactionStack[this.state.locationStackId];
     const dateHash = transactionStack[this.state.dateStackId];
 
-    // console.log(transactions[txHash])
-    // if transaction hash does not exist, don't display anything
+    // if transaction hashes do not exist, don't display anything
     if (!locationHash || !dateHash) return [];
 
-    console.log('hash data');
     // otherwise, return the transaction status
     if (transactions[locationHash] && transactions[locationHash].receipt && transactions[dateHash] && transactions[dateHash].receipt){
       const locations = transactions[locationHash].receipt.events.History.returnValues.arr.slice(1);
@@ -132,14 +62,10 @@ class History extends React.Component {
 
 
     render() {
-      //'rgb(45,156,219)'
       return (
       <View style={styles.container}>
         <NavigationEvents
           onWillFocus={payload => this.getHistory(payload)}
-          onDidFocus={payload => console.log('did focus',payload)} // 
-          onWillBlur={payload => console.log('will blur',payload)}
-          onDidBlur={payload => console.log('did blur',payload)}
         />
         <Title>Details for: item name</Title>
         <Timeline 
